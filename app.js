c578edfaa3856c2a625a32016d71e05bda36f370
Tun Huang <huang.tun.anton@gmail.com>
2022-11-23 05:06:16 +0100

    Create server

---
+++ app.js
@@ -0,0 +1,22 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+import express from 'express';
+
+const PORT = process.env.PORT;
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("hallo")
+});
+
+app.post("/", (req, res) => {
+  res.send(req.body);
+})
+
+app.listen(PORT, () => {
+  console.log("yoo");
+});
